2a9b6153cabbd740a466801d239b22091925059e
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-09-17 21:58:49 -0300

    Criando tabela hash para armazenar tokens

---
+++ src/tokenTable.ts
@@ -0,0 +1,21 @@
+import { Token } from "./token";
+
+export class HashTable {
+  table: Map<string, Array<Token>>;
+
+  constructor() {
+    this.table = new Map();
+  }
+
+  add(token: Token): number | undefined {
+    if (!this.table.get(token.lexema)) {
+      this.table.set(token.lexema, new Array<Token>());
+    }
+
+    return this.table.get(token.lexema)?.push(token);
+  }
+
+  get(lexema: string): Token[] | undefined {
+    return this.table.get(lexema);
+  }
+}
