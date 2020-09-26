719e234a727bd5ca3924eb6359f653c84439268d
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-09-26 18:20:52 -0300

    Formatando arquivos

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+
 program
   .version("0.0.1")
+  .name("mgolc")
+  .usage("[options] file")
   .description("Compilador MGOL")
   .option("-o, --output <file>", "output file name", "PROGRAMA.C")
   .parse(process.argv);
