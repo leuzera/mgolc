aa5fda482e62cb74f7b12b80348fc4df1c7dea77
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-09-26 18:06:18 -0300

    Adicionando commander para cli

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("Hello world!");
+import { program } from "commander";
+program
+  .version("0.0.1")
+  .description("Compilador MGOL")
+  .option("-o, --output <file>", "output file name", "PROGRAMA.C")
+  .parse(process.argv);
+
+console.log(program.opts());
+console.log(program.args);
