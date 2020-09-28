0a0fe8fe65fe32f3dc9728b8ee44b33a4831b023
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-09-27 23:06:30 -0300

    Checando argumentos do programa

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import { exit } from "process";
 
 program
   .version("0.0.1")
@@ -9,6 +10,16 @@ program
   .description("Compilador MGOL")
   .option("-o, --output <file>", "output file name", "PROGRAMA.C")
   .parse(process.argv);
+
+// Se nenhum argumento for passado, o arquivo `FONT.ALG` será usado por padrão
+if (program.args.length === 0) {
+  program.args.push("FONT.ALG");
+}
+
+// Se tiver mais de 1 argumento, termine o programa com erro.
+if (program.args.length > 1) {
+  exit(1);
+}
 
 console.log(program.opts());
 console.log(program.args);
