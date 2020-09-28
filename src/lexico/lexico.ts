158c1999363ce4b41e57a6fe4edf0d98d3ddefb6
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-09-28 00:13:52 -0300

    Lendo arquivo fonte

--- src/lexico/lexico.ts
+++ src/lexico/lexico.ts
@@ -1,11 +1,16 @@
 import fs from "fs";
 import { CharUtils } from "../util";
+import { Token, TOKEN } from "./token";
+import { TokenTable } from "./tokenTable";
+import { TokenErro, ERRO } from "./erro";
+import { tokenMachine } from "./fsm";
 
 export class Lexico {
   path: string;
   posicao: number;
   linha: number;
   coluna: number;
+  tabela: TokenTable;
 
   constructor(path: string) {
     this.path = path;
@@ -13,13 +18,32 @@ export class Lexico {
     this.posicao = 0;
     this.linha = 0;
     this.coluna = 0;
+
+    this.tabela = new TokenTable();
   }
 
-  scan(): null {
-    return null;
+  protected parseLine(line: string): void {
+    for (const char of line) {
+      console.log(char);
+    }
   }
 
-  next(): null {
-    return null;
+  scan(): Lexico {
+    try {
+      const data = fs.readFileSync(this.path, "utf-8");
+      const lines = data.split(/\r?\n/);
+
+      for (const line of lines) {
+        this.parseLine(line);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    return this;
+  }
+
+  next(): Token | undefined {
+    return this.tabela.get(1)?.pop();
   }
 }
