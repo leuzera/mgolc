af7d7a6c0344c56d84ebe7ff838ea402f37308f6
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-10-02 22:47:14 -0300

    Lendo e imprimindo os lexemas

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,16 @@ import { argumentos } from "./util";
 import { Lexico } from "./lexico";
 
 const lexico = new Lexico(argumentos.args[0]).scan();
+
+console.log("[L,C] <TOKEN, TIPO> Lexema");
+
+// eslint-disable-next-line no-constant-condition
+while (true) {
+  const token = lexico.next();
+
+  if (!token) {
+    break;
+  }
+
+  console.info(token.toString());
+}
