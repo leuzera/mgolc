76d919ddac3119c52b975ede30dcd3d177bbfd05
Leonardo Fleury <fleuryleomoraes@gmail.com>
2020-10-02 22:48:55 -0300

    Adicionando palavras reservadas

--- src/lexico/token.ts
+++ src/lexico/token.ts
@@ -1,20 +1,35 @@
 export enum TOKEN {
-  NUM = "NUM", // Constante numérica
-  LITERAL = "LITERAL", // Constante literal
-  ID = "ID", // Identificador
-  COMENTARIO = "COMENTARIO", // Ignorar  comentários, ou seja, reconhecer mas não retornar o token.
-  EOF = "EOF", // Final de Arquivo
-  OPR = "OPR", // Operadores relacionais
-  RCB = "RCB", // Atribuição
-  OPM = "OPM", // Operadores aritméticos
-  AB_P = "AB_P", // Abre Parênteses
-  FC_P = "FC_P", // Fecha Parênteses
-  PT_V = "PT_V", // Ponto e vírgula
-  ERRO = "ERRO", // Qualquer coisa diferente de qualquer símbolo token e palavra-chave definida.
+  NUM = "num", // Constante numérica
+  LITERAL = "literal", // Constante literal
+  ID = "id", // Identificador
+  COMENTARIO = "comentario", // Ignorar  comentários, ou seja, reconhecer mas não retornar o token.
+  EOF = "eof", // Final de Arquivo
+  OPR = "opr", // Operadores relacionais
+  RCB = "rcb", // Atribuição
+  OPM = "opm", // Operadores aritméticos
+  AB_P = "ab_p", // Abre Parênteses
+  FC_P = "fc_p", // Fecha Parênteses
+  PT_V = "pt_v", // Ponto e vírgula
+  ERRO = "erro", // Qualquer coisa diferente de qualquer símbolo token e palavra-chave definida.
+}
+
+export enum RESERVADAS {
+  INICIO = "inicio",
+  VARINICIO = "varinicio",
+  VARFIM = "varfim",
+  ESCREVA = "escreva",
+  LEIA = "leia",
+  SE = "se",
+  ENTAO = "entao",
+  FIMSE = "fimse",
+  FIM = "fim",
+  INTEIRO = "inteiro",
+  LIT = "lit",
+  REAL = "real",
 }
 
 export class Token {
-  token: TOKEN;
+  token: TOKEN | RESERVADAS;
   lexema: string;
   linha: number;
   coluna?: number;
@@ -28,7 +43,7 @@ export class Token {
    * @param coluna Coluna do primeiro caractere do lexema
    * @param tipo Tipo do lexema
    */
-  constructor(token: TOKEN, lexema: string, linha: number, coluna?: number, tipo?: string) {
+  constructor(token: TOKEN | RESERVADAS, lexema: string, linha: number, coluna?: number, tipo?: string) {
     this.token = token;
     this.lexema = lexema;
     this.linha = linha;
@@ -37,6 +52,37 @@ export class Token {
   }
 
   toString(): string {
-    return `<${this.token}, ${this.lexema}, ${this.tipo}>`;
+    return `[${this.linha},${this.coluna}] <${this.token}, ${this.tipo}> ${this.lexema} `;
+  }
+
+  static getReservada(palavra: string): RESERVADAS | undefined {
+    switch (palavra) {
+      case "inicio":
+        return RESERVADAS.INICIO;
+      case "varinicio":
+        return RESERVADAS.VARINICIO;
+      case "varfim":
+        return RESERVADAS.VARFIM;
+      case "escreva":
+        return RESERVADAS.ESCREVA;
+      case "leia":
+        return RESERVADAS.LEIA;
+      case "se":
+        return RESERVADAS.SE;
+      case "entao":
+        return RESERVADAS.ENTAO;
+      case "fimse":
+        return RESERVADAS.FIMSE;
+      case "fim":
+        return RESERVADAS.FIM;
+      case "inteiro":
+        return RESERVADAS.INTEIRO;
+      case "lit":
+        return RESERVADAS.LIT;
+      case "real":
+        return RESERVADAS.REAL;
+      default:
+        return undefined;
+    }
   }
 }
